b2be4a8e51025a16e2ac11c668721d3112fe9558
Axel546 <62812228+Axel546@users.noreply.github.com>
2021-06-20 20:35:49 +0300

    Nou

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 new fullpage('#fullpage', {
 	//options here
     autoScrolling:true,
-    sectionsColor: ['#68b0ab', '#8fc0a9', '#c8d5b9', '#faf3dd'],
+    sectionsColor: ['#003B46', '#07575B', '#66A5AD', '#C4DFE6'],
     navigation: true,
     onLeave: (origin,destination,direction)=> {
         const section = destination.item;
         console.log(section);
-        TweenMax.fromTo(section, 1, {opacity: 0}, {opacity: 1},'+=700');
+        TweenMax.fromTo(section, 1, {opacity: 0}, {opacity: 1},'+=500');
     }
 });
 
